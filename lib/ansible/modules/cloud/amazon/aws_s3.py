a4853aa915a395766c4f1b7a94de0523d48c00af
Paul Arthur <paul.arthur@flowerysong.com>
2018-04-29 03:54:58 +0000

    Move S3 metadata dict parsing into a utility function

--- lib/ansible/modules/cloud/amazon/aws_s3.py
+++ lib/ansible/modules/cloud/amazon/aws_s3.py
@@ -283,6 +283,7 @@ from ansible.module_utils.six.moves.urllib.parse import urlparse
 from ssl import SSLError
 from ansible.module_utils.basic import AnsibleModule, to_text, to_native
 from ansible.module_utils.ec2 import ec2_argument_spec, camel_dict_to_snake_dict, get_aws_connection_info, boto3_conn, HAS_BOTO3
+from ansible.module_utils.aws.s3 import dict_to_s3_extra_args
 
 try:
     import botocore
@@ -460,21 +461,6 @@ def path_check(path):
         return False
 
 
-def option_in_extra_args(option):
-    temp_option = option.replace('-', '').lower()
-
-    allowed_extra_args = {'acl': 'ACL', 'cachecontrol': 'CacheControl', 'contentdisposition': 'ContentDisposition',
-                          'contentencoding': 'ContentEncoding', 'contentlanguage': 'ContentLanguage',
-                          'contenttype': 'ContentType', 'expires': 'Expires', 'grantfullcontrol': 'GrantFullControl',
-                          'grantread': 'GrantRead', 'grantreadacp': 'GrantReadACP', 'grantwriteacp': 'GrantWriteACP',
-                          'metadata': 'Metadata', 'requestpayer': 'RequestPayer', 'serversideencryption': 'ServerSideEncryption',
-                          'storageclass': 'StorageClass', 'ssecustomeralgorithm': 'SSECustomerAlgorithm', 'ssecustomerkey': 'SSECustomerKey',
-                          'ssecustomerkeymd5': 'SSECustomerKeyMD5', 'ssekmskeyid': 'SSEKMSKeyId', 'websiteredirectlocation': 'WebsiteRedirectLocation'}
-
-    if temp_option in allowed_extra_args:
-        return allowed_extra_args[temp_option]
-
-
 def upload_s3file(module, s3, bucket, obj, src, expiry, metadata, encrypt, headers):
     if module.check_mode:
         module.exit_json(msg="PUT operation skipped - running in check mode", changed=True)
@@ -483,15 +469,8 @@ def upload_s3file(module, s3, bucket, obj, src, expiry, metadata, encrypt, heade
         if encrypt:
             extra['ServerSideEncryption'] = 'AES256'
         if metadata:
-            extra['Metadata'] = {}
-
             # determine object metadata and extra arguments
-            for option in metadata:
-                extra_args_option = option_in_extra_args(option)
-                if extra_args_option is not None:
-                    extra[extra_args_option] = metadata[option]
-                else:
-                    extra['Metadata'][option] = metadata[option]
+            extra.update(dict_to_s3_extra_args(metadata))
 
         if 'ContentType' not in extra:
             content_type = mimetypes.guess_type(src)[0]
